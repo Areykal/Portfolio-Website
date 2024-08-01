da6e0e50fea1ca470403aa616ddada3aabdd6c22
Areykal <areykalkh@gmail.com>
2024-08-01 21:48:27 +0700

    fix error logged to console

--- vite.config.js
+++ vite.config.js
@@ -13,18 +13,7 @@ export default defineConfig({
         short_name: "Areykal Portfolio",
         description: "Portfolio website of Areykal Ho",
         theme_color: "#ffffff",
-        icons: [
-          {
-            src: "pwa-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
+        icons: [],
       },
     }),
   ],
